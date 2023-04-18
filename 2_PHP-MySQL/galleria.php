ddc96ec2394a5d7a6a69ac8c93f7728baee40f42
Hackfront <hackfront@protonmail.ch>
2023-04-19 00:32:39 +0200

    Aggiunte dimensioni immagini in galleria.php

--- 2_PHP-MySQL/galleria.php
+++ 2_PHP-MySQL/galleria.php
@@ -1,3 +1,17 @@
+<?php
+$dim_img = [
+  [   '0',    '0'],
+  ['1200',  '630'],
+  [ '300',  '300'],
+  [ '660',  '450'],
+  ['1024',  '683'],
+  ['1024',  '621'],
+  ['1920', '1080'],
+  ['1920', '1080'],
+];
+
+?>
+
 <?xml version="1.0" encoding="UTF-8" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
@@ -57,7 +71,7 @@
         </div>
       </div>
     </div>
-    
+
     <?php
       $cartella_immagini = 'res';
       $rdi = new RecursiveDirectoryIterator ( $cartella_immagini );
@@ -72,8 +86,8 @@
           'jpeg' => 'image/jpeg',
           'jpg' => 'image/jpeg',
           'gif' => 'image/gif',
-        ) ) ) { 
-          continue; 
+        ) ) ) {
+          continue;
         }
         echo '<p>' . $file->getFilename() . '</p>';
       }
