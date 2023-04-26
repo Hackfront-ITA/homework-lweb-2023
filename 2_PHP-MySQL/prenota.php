ae14f26b76ddbb5365a04ba5288ec06b6abfd0d2
Hackfront <hackfront@protonmail.ch>
2023-04-26 16:40:17 +0200

    Aggiunta prenotazione a prenota.php

--- 2_PHP-MySQL/prenota.php
+++ 2_PHP-MySQL/prenota.php
@@ -1,5 +1,39 @@
 <?php
-$posti = 0;
+require_once("connessione.php");
+
+$conn_db = connessione_db();
+
+const NUM_MAX_POSTI = 30;
+
+if (isset($_POST['azione']) && $_POST['azione'] === 'prenota') {
+  $query  = sprintf(
+    "INSERT INTO %s (nome, cognome, corso) VALUES ('%s', '%s', '%s')",
+    TBL_PRENOTAZIONI, $_POST['nome'], $_POST['cognome'], $_POST['corso']
+  );
+  $result = mysqli_query($conn_db, $query);
+  if (!$result) {
+    printf("Errore nella query.\n");
+    exit();
+  }
+
+  $prenotato = true;
+} else if (isset($_GET['corso'])) {
+  $query  = "SELECT COUNT(*) AS num FROM " . TBL_PRENOTAZIONI;
+  $query .= " WHERE corso = '" . $_GET['corso'] . "'";
+  $result = mysqli_query($conn_db, $query);
+  if (!$result) {
+    printf("Errore nella query.\n");
+    exit();
+  }
+
+  $row = mysqli_fetch_assoc($result);
+
+  $posti = NUM_MAX_POSTI - $row['num'];
+
+  $prenotato = false;
+} else {
+  exit();
+}
 ?>
 
 <?xml version="1.0" encoding="UTF-8" ?>
@@ -31,8 +65,9 @@ $posti = 0;
 
   <div id="contenuto" class="centrato">
     <h2 class="pb-16">PRENOTAZIONE</h2>
+<?php if (!$prenotato) { ?>
     <p class="pb-16">Posti disponibili: <?php echo($posti); ?></p>
-<?php if ($posti > 0) { ?>
+<?php   if ($posti > 0) { ?>
     <form action="prenota.php" method="POST">
       <label for="nome">Nome:</label><br>
       <input type="text" id="nome" name="nome"><br><br>
@@ -40,10 +75,17 @@ $posti = 0;
       <label for="cognome">Cognome:</label><br>
       <input type="text" id="cognome" name="cognome"><br><br>
 
-      <input type="submit" value="Invia">
+      <input type="hidden" name="corso" value="<?php echo ($_GET['corso']); ?>" />
+      <button type="submit" name="azione" value="prenota" class="button">Prenota</button>
     </form>
-<?php } else { ?>
+<?php   } else { ?>
     <h2>Prenotazioni chiuse!</h2>
+<?php   } ?>
+<?php } else { ?>
+    <p class="pb-16"><b>La prenotazione &egrave; andata a buon fine.</b></p>
+    <div class="mt-32 mb-8">
+      <a class="button" href="corsi.html">Indietro</a>
+    </div>
 <?php } ?>
   </div>
 
