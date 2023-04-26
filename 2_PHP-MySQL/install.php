106859692726bd4d79a3883bba9406b573f75a42
Hackfront <hackfront@protonmail.ch>
2023-04-26 15:31:56 +0200

    Sistemati script installazione e connessione db

--- 2_PHP-MySQL/install.php
+++ 2_PHP-MySQL/install.php
@@ -1,55 +1,56 @@
 <?php
 require_once("connessione.php");
 
-$conn = connessione_db();
+/*** Creazione database ***/
+$conn_db = connessione_db(true);  // true: connessione senza database
 
-$creazione_db = "CREATE DATABASE IF NOT EXISTS " . DB_NAME;
-if(!mysqli_query($conn, $creazione_db)) {
+$query = "CREATE DATABASE IF NOT EXISTS " . DB_NAME;
+if (!mysqli_query($conn_db, $query)) {
   printf("Problemi nella creazione del database.");
+  exit();
 }
 
-$conn_db = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conn_db = connessione_db();
 
-if (mysqli_connect_errno()){
-  printf("Errore nella connessione al database %s: %s\n", DB_NAME, mysqli_connect_error($conn_db));
+/*** Creazione tabella prodotti ***/
+$query  = "CREATE TABLE IF NOT EXISTS " . TBL_PRODOTTI . " (";
+$query .= "  id     INT          NOT NULL AUTO_INCREMENT, ";
+$query .= "  nome   VARCHAR(50)  NOT NULL, ";
+$query .= "  prezzo DECIMAL(5,2) NOT NULL, ";
+$query .= "  PRIMARY KEY(id), ";
+$query .= "  UNIQUE(nome)";
+$query .= ");";
+
+if(!mysqli_query($conn_db, $query)){
+    printf("Problemi nella creazione della tabella %s.\n", TBL_PRODOTTI);
+    exit();
 }
 
-$prodotto = "prodotto";
-$query_creazione = "CREATE TABLE IF NOT EXISTS $prodotto (";
-$query_creazione .= "id int NOT NULL AUTO_INCREMENT, ";
-$query_creazione .= "nome varchar (50) NOT NULL, ";
-$query_creazione .= "prezzo decimal(5,2) NOT NULL, ";
-$query_creazione .= "PRIMARY KEY(id), ";
-$query_creazione .= "UNIQUE (nome)";
-$query_creazione .= ");";
+/*** Inserimento tabella prodotti ***/
+$check = "SELECT * FROM " . TBL_PRODOTTI;
+$result = mysqli_query($conn_db, $check);
 
-if(!mysqli_query($conn_db, $query_creazione)){
-    printf("Problemi nella creazione della tabella %s.\n", $prodotto);
-}
+if (!mysqli_fetch_assoc($result)) {
+  $query  = "INSERT INTO " . TBL_PRODOTTI . " (nome, prezzo) VALUES ";
+  $query .= "  (\"BLP99.9 8:1:1 Pure Professional\", 39.95), ";
+  $query .= "  (\"Omega Pure\", 19.99), ";
+  $query .= "  (\"100% Whey 2,28kg\", 100.99), ";
+  $query .= "  (\"Isolate-pro grass-fed 700g\", 52.00), ";
+  $query .= "  (\"Snickers hi protein 62g\", 2.70), ";
+  $query .= "  (\"Iso Whey Zero\", 49.87), ";
+  $query .= "  (\"Choco Egg 40gg\", 2.40), ";
+  $query .= "  (\"Vb whey 104 9.8 - 450g\", 46.00), ";
+  $query .= "  (\"Vb whey 104 9.8 - 900g\", 76.00), ";
+  $query .= "  (\"Whey 80 professional 2000g\", 70.99), ";
+  $query .= "  (\"Mars protein 57g\", 2.99), ";
+  $query .= "  (\"Hydro Purebar 55g\", 5.00), ";
+  $query .= "  (\"Grenade Carb Killa 60g\", 3.95), ";
+  $query .= "  (\"CarbControl Bar 100g\", 5.70), ";
+  $query .= "  (\"Peanut Butter 495g\", 9.99);";
 
-$controllo_popolamento = "SELECT * FROM $prodotto";
-$result = mysqli_query($conn_db, $controllo_popolamento);
-
-if(!mysqli_fetch_assoc($result)){
-  $query_ins = "INSERT INTO $prodotto (nome, prezzo) VALUES ";
-  $query_ins .= "(\"BLP99.9 8:1:1 Pure Professional\", 39.95), ";
-  $query_ins .= "(\"Omega Pure\", 19.99), ";
-  $query_ins .= "(\"100% Whey 2,28kg\", 100.99), ";
-  $query_ins .= "(\"Isolate-pro grass-fed 700g\", 52.00), ";
-  $query_ins .= "(\"Snickers hi protein 62g\", 2.70), ";
-  $query_ins .= "(\"Iso Whey Zero\", 49.87), ";
-  $query_ins .= "(\"Choco Egg 40gg\", 2.40), ";
-  $query_ins .= "(\"Vb whey 104 9.8 - 450g\", 46.00), ";
-  $query_ins .= "(\"Vb whey 104 9.8 - 900g\", 76.00), ";
-  $query_ins .= "(\"Whey 80 professional 2000g\", 70.99), ";
-  $query_ins .= "(\"Mars protein 57g\", 2.99), ";
-  $query_ins .= "(\"Hydro Purebar 55g\", 5.00), ";
-  $query_ins .= "(\"Grenade Carb Killa 60g\", 3.95), ";
-  $query_ins .= "(\"CarbControl Bar 100g\", 5.70), ";
-  $query_ins .= "(\"Peanut Butter 495g\", 9.99)";
-
-  if(!mysqli_query($conn_db, $query_ins)){
-    printf("Problemi nell'inserimento dei dati nella tabella %s.\n", $prodotto);
+  if (!mysqli_query($conn_db, $query)) {
+    printf("Problemi nell'inserimento dei dati nella tabella %s.\n", $tbl_prodotti);
+    exit();
   }
 }
 
