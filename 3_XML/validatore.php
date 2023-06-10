4070833ecb37fc3bde7d1de37a2f575070b3d1e1
Hackfront <hackfront@protonmail.ch>
2023-06-10 13:20:59 +0200

    Sistemato validatore.php

--- 3_XML/validatore.php
+++ 3_XML/validatore.php
@@ -1,13 +1,9 @@
 <?php
 $doc = new DOMDocument();
-$rawContent = file('./xml/ordini.xml');
-$xmlString = implode("\n", $rawContent);
-/*
 $xmlString = '';
 foreach(file('./xml/ordini.xml') as $line) {
   $xmlString .= trim($line);
 }
-*/
 $doc->loadXML($xmlString);
 $result = $doc->schemaValidate('./xml/ordini.xsd');
 if ($result) {
