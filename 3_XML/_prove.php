410f8d822c23d88c116a2e0b9c3f039a62ecd0f6
Hackfront <hackfront@protonmail.ch>
2023-06-10 13:21:13 +0200

    Aggiunte prove visualizzazione

---
+++ 3_XML/_prove.php
@@ -0,0 +1,31 @@
+<?php
+$doc = new DOMDocument();
+$xmlString = '';
+foreach(file('./xml/ordini.xml') as $line) {
+  $xmlString .= trim($line);
+}
+$doc->loadXML($xmlString);
+$root = $doc->documentElement;
+
+$ordini = $root->childNodes;
+for ($i = 0; $i < $ordini->length; $i++) {
+  $ordine = $ordini->item($i);
+  $id_ordine = $ordine->getAttribute('id');
+  echo ("Ordine $id_ordine:\n");
+
+  $utente = $ordine->getElementsByTagName('utente')[0]->textContent;
+  echo("  Utente: $utente\n");
+
+  $indirizzo = $ordine->getElementsByTagName('indirizzo')[0]->textContent;
+  echo("  Indirizzo: $indirizzo\n");
+
+  $articoli = $ordine->getElementsByTagName('articoli')[0]->childNodes;
+  echo("  Articoli:\n");
+  for ($j = 0; $j < $articoli->length; $j++) {
+    $articolo = $articoli->item($j);
+    $id_articolo = $articolo->getAttribute('id');
+    $quantita = $articolo->getAttribute('quantita');
+    echo("    ID: $id_articolo, quantita: $quantita\n");
+  }
+}
+?>
